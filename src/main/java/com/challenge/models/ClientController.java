c04fb6031f0f9f1be7ae93276a9b2e40c3a8dcd4
Jorge Meza Bello <jorberiver@gmail.com>
2023-05-24 08:44:10 -0600

    Endpoints completados

--- src/main/java/com/challenge/models/ClientController.java
+++ src/main/java/com/challenge/models/ClientController.java
@@ -3,12 +3,19 @@ package com.challenge.models;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.challenge.service.IClientService;
+import com.nimbusds.oauth2.sdk.Response;
 
 @RestController
 @RequestMapping("api/clients")
@@ -23,6 +30,32 @@ public class ClientController {
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<T>
+	public ResponseEntity<Client> getClienteById(@PathVariable("id") Integer id){
+		return clienteService.findById(id)
+				.map( ResponseEntity::ok)
+				.orElseGet( () -> ResponseEntity.notFound().build());
+	}
+	@PostMapping("/create")
+	public ResponseEntity<Client> createNewClient(@RequestBody Client client){
+		return new ResponseEntity<>(clienteService.create(client), HttpStatus.CREATED);
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<Client> updateClient(@RequestBody Client client, @PathVariable("id") Integer id){
+		return clienteService.findById(id)
+				.map( cli -> ResponseEntity.ok(clienteService.update(client) ))
+				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Client> deleteClientById(@PathVariable("id") Integer id){
+		return clienteService.findById(id)
+				.map( cli -> {
+						clienteService.delete(id);
+						return ResponseEntity.ok(cli); 
+						})
+				.orElseGet(() -> ResponseEntity.notFound().build());
+				 		 		
+				
+	}
+	
 
 }
